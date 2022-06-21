9aa4959a21c29288a1f5bb982c89dc59cb0609c6
Abhijeet Mukherjee <104889431+AbhijeetMukherjee@users.noreply.github.com>
2022-06-20 22:53:44 -0700

    Add files via upload

---
+++ video to audio converter.py
@@ -0,0 +1,12 @@
+import imp
+
+
+import moviepy.editor
+from tkinter.filedialog import *
+vid = askopenfilename()
+video = moviepy.editor.VideoFileClip(vid)
+
+aud = video.audio
+aud.write_audiofile("demo.mp3")
+print("completed")
+
